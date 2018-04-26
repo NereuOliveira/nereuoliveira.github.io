dec1a2b1b142a63c828df9049057a5b8776d134b
Nereu Oliveira <me@nereu.it>
2018-04-25 20:52:14 -0400

    Batman! (this commit has no parents)

---
+++ assets/data/experience.js
@@ -0,0 +1,35 @@
+let experience =
+[
+    {
+        "position": "Software Engineer",
+        "company": "SINDESA",
+        "country": "Brazil",
+        "start_date": "2013-10-01",
+        "end_date": "2017-08-01",
+        "description": "Led and assisted three distinct web projects for the company and its clients.\n- Guaranteed exchange of information for monthly transactions above $150,000 USD;\n- Developed and provided support to systems with large amount of critical data;\n- Provided technical leadership for junior developers;\n- Migrated the main company projects to Azure servers for scalability;\n- Created new interfaces to facilitate the user interaction with legacy systems;\n- Created a software solution to resolve a communication deficiency of 25,000 volunteers;\n- Assured the receipt of votes from 4,000 ballot boxes in 1,191 cities on the day of the plebiscite;"
+    },
+    {
+        "position": "Software Developer",
+        "company": "Benner Systems",
+        "country": "Brazil",
+        "start_date": "2013-01-01",
+        "end_date": "2013-09-01",
+        "description": "Provided maintenance and support for the company's main product which was an ERP system. Worked as part of the legal and court demands team interacting with system and business analysts.\n- Participated in monthly projects to adapt the system to legal and court demands;\n- Created and maintained reports using Visual Basic as report engine;\n- Performed code reviews on Pascal, C# and Visual Basic code from others software developers to improve quality;\n- Developed and tested new system functionalities using C#, .NET and the Entity Framework;\n- Participated in the process of migrating legacy code written in Pascal to C#;"
+    },
+    {
+        "position": "Software Developer",
+        "company": "Evolutecnet",
+        "country": "Brazil",
+        "start_date": "2011-10-01",
+        "end_date": "2012-12-01",
+        "description": "Developed a new product focused on reducing clients' rework and provided an easier way of collecting data from the container yard.\n- Programmed a windows mobile application for data collector devices using C# and consuming a PHP RESTful API;\n- Maintained several of the company's desktop modules using Pascal (Delphi);\n- Provided user support;\n- Created reports using FastReport and QuickReport tools;"
+    },
+    {
+        "position": "Software Developer",
+        "company": "Ravex",
+        "country": "Brazil",
+        "start_date": "2010-11-01",
+        "end_date": "2011-09-01",
+        "description": "Developed systems with the focus on innovation in the areas of mobile, embedded system and Web Services. Also participated in phases before the implementation like system analysis definition, technology researching and prototyping.\n- Created Web Services based on the SOAP protocol using C#;\n- Integrated SQL Server databases via Windows Service applications;\n- Developed mobile applications using Java ME (J2ME) and the Android platform;\n- Implemented embedded solutions for PIC microcontrollers using mikroC for PIC;"
+    }
+]
