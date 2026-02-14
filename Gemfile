270ef8fb3589f3689bbf7868938b4032ebec40e6
Nereu Oliveira <nereu.oliveira@outlook.com>
2026-02-14 12:15:28 -0300

    feat(site): migrate resume rendering to Jekyll data and Liquid templates

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'jekyll', '~> 4.3'
+gem 'webrick', '~> 1.8'
+gem 'jekyll-seo-tag', '~> 2.8'
--- assets/resume.js
+++ assets/resume.js
@@ -1,42 +1,31 @@
-var converter = new showdown.Converter();
+(function ($) {
+  'use strict';
 
-Vue.filter('phone', function (phone) {
-    if (phone === undefined)
-        return '';
-
-    return phone.replace(/[^0-9]/g, '')
-        .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
-});
-
-const app = new Vue({
-    el: '#app',
-    data: {
-        about: about,
-        experience: experience,
-        education: education,
-        skills: skills
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          'easeInOutExpo'
+        );
+        return false;
+      }
     }
-});
+  });
 
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: (target.offset().top)
-            }, 1000, "easeInOutExpo");
-            return false;
-        }
-    }
-});
-
-// Closes responsive menu when a scroll trigger link is clicked
-$('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
-});
+  });
 
-// Activate scrollspy to add active class to navbar items on scroll
-$('body').scrollspy({
+  $('body').scrollspy({
     target: '#sideNav'
-});
+  });
+})(jQuery);
